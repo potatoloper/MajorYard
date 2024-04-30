432a7e1350154bbc66fc7abddd6c968a87c24015
qkralstj0808 <qkralstj08@gmail.com>
2024-05-01 01:57:17 +0900

    test3

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,2 +1,28 @@
-package entity;public class User {
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Table(name = "User")
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_no", nullable = false)
+    private Long id;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+
+
 }
