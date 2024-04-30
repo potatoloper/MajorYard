5e604bb4ed6de04729fcf7230d42f655b0fc56cf
qkralstj0808 <qkralstj08@gmail.com>
2024-04-30 20:02:17 +0900

    first commit

---
+++ src/test/java/com/KAU/majorYard/MajorYardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KAU.majorYard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorYardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
