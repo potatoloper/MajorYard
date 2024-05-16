df6074197ad5efaa61e48ae597bad096ff4595df
myosookim <o_rul8_2@naver.com>
2024-05-17 02:23:23 +0900

    Feat : API 테스트용 board.sql, User.sql 리소스 추가.

---
+++ src/main/resources/User.sql
@@ -0,0 +1,12 @@
+insert into User (userName, nickName, gender) values ('Papagena', 'Derham', 'FEMALE');
+insert into User (userName, nickName, gender) values ('Haily', 'Juanes', 'MALE');
+insert into User (userName, nickName, gender) values ('Loydie', 'Dougharty', 'MALE');
+insert into User (userName, nickName, gender) values ('Susanne', 'Paslow', 'FEMALE');
+insert into User (userName, nickName, gender) values ('Conroy', 'Indgs', 'MALE');
+insert into User (userName, nickName, gender) values ('Orrin', 'Guilliatt', 'MALE');
+insert into User (userName, nickName, gender) values ('Stevy', 'Barajas', 'MALE');
+insert into User (userName, nickName, gender) values ('Jessy', 'Simyson', 'FEMALE');
+insert into User (userName, nickName, gender) values ('Joaquin', 'Gormally', 'MALE');
+insert into User (userName, nickName, gender) values ('Woodie', 'Heball', 'MALE');
+
+commit;
