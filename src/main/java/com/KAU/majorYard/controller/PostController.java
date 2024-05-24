9faaaa7a7400d8f4061728a86004e9fbb4e1a135
qkralstj0808 <qkralstj08@gmail.com>
2024-05-24 22:46:39 +0900

    Fix: Controller 명 수정

--- src/main/java/com/KAU/majorYard/controller/PostController.java
+++ src/main/java/com/KAU/majorYard/controller/PostController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 //@RequestMapping(path = "/")
-public class PostApiController {
+public class PostController {
 
     private final PostServiceImpl postService;
 
