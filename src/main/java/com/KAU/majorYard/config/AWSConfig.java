6211cd59986f9d16d8d2a0a017423b4c07025c02
myosookim <o_rul8_2@naver.com>
2024-06-02 18:32:20 +0900

    Feat : 이미지 S3 저장소와 연동

--- src/main/java/com/KAU/majorYard/config/AWSConfig.java
+++ src/main/java/com/KAU/majorYard/config/AWSConfig.java
@@ -1,5 +1,9 @@
 package com.KAU.majorYard.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +21,18 @@ public class AWSConfig {
 
     @Value("${spring.cloud.aws.region.static}")
     private String region;
+
+    // S3
+    @Bean
+    public AmazonS3Client amazonS3Client(){
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, SecretKey);
+
+        return (AmazonS3Client) AmazonS3ClientBuilder
+                .standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .build();
+    }
 
     @Bean
     public S3Client s3Client() {
