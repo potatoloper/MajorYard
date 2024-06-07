1bb58e4b9a0b060c692f784316571cb9fa8f9529
myosookim <o_rul8_2@naver.com>
2024-06-07 22:52:24 +0900

    Fix : 게시글 조회에서 스터디게시판 특화 컬럼 추가

--- src/main/java/com/KAU/majorYard/dto/response/PostPagingResponseDto.java
+++ src/main/java/com/KAU/majorYard/dto/response/PostPagingResponseDto.java
@@ -22,16 +22,27 @@ public class PostPagingResponseDto {
     private int postScrab;
     private int postcomment;
 
+    //홍보게시판 특화
     @Enumerated(value = EnumType.STRING)
     private PostType postType;
+
+    //질문게시판 특화
     @Enumerated(value = EnumType.STRING)
     private Answered answered;
+
+    /* 스터디게시판 특화 */
+    private String studyRegion;
+    private Integer studyPartyOf;
+    private Integer studyFee;
+    private String studyUntil;
+
+    //시사이슈게시판 특화
+    private String contentUrl;
 
     private String nickName;
     //@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss", timezone = "Asia/Seoul")
     private String modifiedTime;
     private List<String> imgUrls;
-    private String contentUrl;
 
     // 게시판별 게시글 페이징 조회 response
     @Builder
@@ -44,6 +55,10 @@ public class PostPagingResponseDto {
         this.postcomment = post.getPostcomment();
         this.postType = post.getPostType();
         this.answered = post.getAnswered();
+        this.studyRegion = post.getStudyRegion();
+        this.studyPartyOf = post.getStudyPartyOf();
+        this.studyFee = post.getStudyFee();
+        this.studyUntil = post.getStudyUntil();
         this.nickName = post.getUser().getNickName();
         this.modifiedTime = post.getModifiedDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         this.imgUrls = imgUrls;
