14093b38d2e8dc7ea6e3a18cc09cc21036baf494
myosookim <o_rul8_2@naver.com>
2024-06-15 17:35:57 +0900

    Feat : 로그인 리스폰스에 에러코드 추가

--- src/main/java/com/KAU/majorYard/dto/CommonRestResult.java
+++ src/main/java/com/KAU/majorYard/dto/CommonRestResult.java
@@ -11,10 +11,13 @@ public class CommonRestResult{
     public enum CommonRestResultEnum {
         PASS("200", "수행 성공"),
         SAVE_SUCCESS("201", "저장 성공"),
+        SINGUP_SUCCESS("2000", "회원가입 성공"),
         PASS_ERROR("400", "수행 실패"),
         SAVE_ERROR("401", "저장 실패"),
         LIKE_DUPLICATE("1001", "게시글에 대한 유저의 중복된 좋아요 요청"),
-        SCRAB_DUPLICATE("1101", "게시글에 대한 유저의 중복된 스크랩 요청");
+        SCRAB_DUPLICATE("1101", "게시글에 대한 유저의 중복된 스크랩 요청"),
+        LOGIN_ID_DUPLICATE("2001", "로그인 아이디 중복"),
+        NICKNAME_DUPLICATE("2002", "닉네임 중복");
 
 
         private String code;
