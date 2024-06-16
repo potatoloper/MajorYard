dc1555da8dab2170fcdb5948c62d5187fb53db62
Jae Hyeok Kim <102157301+jaehhhk@users.noreply.github.com>
2024-06-17 05:43:06 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
 
 #s
 echo "> build 파일 복사" >> /home/ubuntu/deploy.log
-DEPLOY_PATH=/home/ubuntu/MajorYard/
+DEPLOY_PATH=/home/ubuntu/
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
